a3b988a1457daf6f80b23aed7947f55e2f4db898
Krishna <krishna9842740994@gmail.com>
2026-05-13 21:26:05 +0530

    added login page

--- validation.js
+++ validation.js
@@ -3,6 +3,7 @@ const firstname_input=document.getElementById('firstname-input');
 const email_input=document.getElementById('email-input');
 const password_input=document.getElementById('password-input');
 const repeat_password_input=document.getElementById('repeat-password-input');
+const error_message=document.getElementById('error-message');
 
 
 form.addEventListener('submit',(e)=>{
@@ -23,6 +24,9 @@ form.addEventListener('submit',(e)=>{
 
     if(errors.length>0){
         e.preventDefault();
+
+        error_message.innerText=errors.join(". ");
+        error
     }
 });
 
@@ -40,10 +44,50 @@ if(email==='' || email===null){
 
 
 if(password==='' || password===null){
-    errors.push('password is required');
+    errors.push('Password is required');
     password_input.parentElement.classList.add('incorrect');
+}
+
+if(password.length<8){
+    errors.push('Password must be at least 8 characters long');
+    password_input.parentElement.classList.add('incorrect');
+}
+if(password !== repeat_password){
+    errors.push('Password do not match');
+    password_input.parentElement.classList.add('incorrect');
+    repeat_password_input.parentElement.classList.add('incorrect');
+
 }
 return errors;
 }
 
 
+function getLoginFormErrors(email,password){
+    let errors=[];
+
+    if(email==='' || email===null){
+    errors.push('Email is required');
+    email_input.parentElement.classList.add('incorrect');
+}
+
+
+if(password==='' || password===null){
+    errors.push('Password is required');
+    password_input.parentElement.classList.add('incorrect');
+}
+
+return errors;
+}
+
+
+const allInputs=[firstname_input,email_input,password_input,repeat_password_input].filter(input=>input!==null);  // filtering out the null input elements
+
+allInputs.forEach(input=>{
+    input.addEventListener('input',()=>{
+        if(input.parentElement.classList.contains('incorrect')) {
+            input.parentElement.classList.remove('incorrect');
+
+            error_message.innerText='';
+        }
+    })
+})
